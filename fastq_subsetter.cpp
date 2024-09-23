097c9e654e111a565dfdad78a6489dc2037e8f7f
Evan P. Troendle <e.troendle@qub.ac.uk>
2024-09-23 11:49:37 +0100

    Added the following features: 1. Ratio-based Subsampling: The new subsample_fastq_ratio function allows users to specify a ratio for how much of the input should be sampled.
    2. Streamlined Argument Parsing:
    The logic for argument parsing has been streamlined. 
    
    3. Plans for future improvement - have  created (and will utilise in future) a cache (unordered_map) for loading file line numbers only once  in 'range'-mode. presently, it wastefully re-counts the number of lines in the input file...

--- fastq_subsetter.cpp
+++ fastq_subsetter.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
 #include <set>
 #include <string>
@@ -9,6 +7,7 @@
 #include <thread>
 #include <future>
 #include <regex>
+#include <unordered_map>
 #include <dirent.h>
 #include <zlib.h>
 #include <unistd.h>
@@ -70,13 +69,13 @@ void iterate_fastq(const std::string& input_path, const std::set<std::size_t>& i
     gzclose(output_file);  // Close the output file when done
 }
 
-void subsample_fastq(const std::string& input, const std::string& output, std::size_t num_reads, unsigned int random_seed = 0) {
+void subsample_fastq(const std::string& input, const std::string& output, const std::size_t& num_reads, std::unordered_map<std::string, std::size_t>& cache, const std::size_t& random_seed = 0) {
     std::size_t total_records = count_reads(input);
-    
-    if (total_records <= num_reads) {
-        throw std::runtime_error("Requested number of reads:" + std::to_string(total_records) + " meets or exceeds available number of reads");
+
+    if (total_records < num_reads) {
+        throw std::runtime_error("Requested number of reads:" + std::to_string(num_reads) + " exceeds available number of reads: " + std::to_string(total_records));
     }
-    
+
     std::vector<std::size_t> indices(total_records);
     std::iota(indices.begin(), indices.end(), 0);
     std::shuffle(indices.begin(), indices.end(), std::default_random_engine(random_seed));
@@ -85,16 +84,39 @@ void subsample_fastq(const std::string& input, const std::string& output, std::s
     iterate_fastq(input, std::set<std::size_t>(indices.begin(), indices.end()), output);
 }
 
-void process_file(const std::string& input_path, const std::string& output_dir, std::size_t num_reads_to_downsample) {
+void process_file_range(const std::string& input_path, const std::string& output_dir, const std::size_t& num_reads_to_downsample, std::unordered_map<std::string, std::size_t>& cache, const std::size_t& seed) {
     std::string file_name = input_path.substr(input_path.find_last_of('/') + 1);
     std::string output_path = output_dir + std::to_string(num_reads_to_downsample) + "_" + file_name;
 
     try {
-        subsample_fastq(input_path, output_path, num_reads_to_downsample, 42);
+        subsample_fastq(input_path, output_path, num_reads_to_downsample, cache, seed);
     } catch (const std::exception& e) {
         std::cerr << "Error processing " << file_name << ": " << e.what() << std::endl;
     }
 }
+
+void subsample_fastq_ratio(const std::string& input, const std::string& output, const double& ratio, const std::size_t& random_seed = 0) {
+    std::size_t total_records = count_reads(input);
+
+    std::vector<std::size_t> indices(total_records);
+    std::iota(indices.begin(), indices.end(), 0);
+    std::shuffle(indices.begin(), indices.end(), std::default_random_engine(random_seed));
+    indices.resize(static_cast<std::size_t>(total_records * ratio));
+
+    iterate_fastq(input, std::set<std::size_t>(indices.begin(), indices.end()), output);
+}
+
+void process_file_ratio(const std::string& input_path, const std::string& output_dir, const double& ratio, const std::size_t& seed ) {
+    std::string file_name = input_path.substr(input_path.find_last_of('/') + 1);
+    std::string output_path = output_dir + "ratio=" + std::to_string(ratio) + "_" + file_name;
+
+    try {
+        subsample_fastq_ratio(input_path, output_path, ratio, seed);
+    } catch (const std::exception& e) {
+        std::cerr << "Error processing " << file_name << ": " << e.what() << std::endl;
+    }
+}
+
 
 std::vector<std::string> getFilesInDirectory(const std::string& directory, const std::regex& pattern) {
     std::vector<std::string> files;
@@ -112,12 +134,14 @@ std::vector<std::string> getFilesInDirectory(const std::string& directory, const
     return files;
 }
 
+
 int main(int argc, char* argv[]) {
-    std::string input_dir = "";
-    std::string output_dir = "";
+    std::string input_dir, output_dir;
     std::vector<std::size_t> reads;
     std::string pattern_str = ".*_R[12]_001\\.fastq(\\.gz)?";
     std::size_t start = 0, stop = 0, step = 0;
+    std::size_t seed = 0;
+    double ratio = 0.0;
     bool force = false;
 
     // Define long-named options
@@ -125,71 +149,17 @@ int main(int argc, char* argv[]) {
         {"in", required_argument, 0, 'i'},
         {"out", required_argument, 0, 'o'},
         {"regex", required_argument, 0, 'p'},
-        {"start", required_argument, 0, 's'},
-        {"stop", required_argument, 0, 't'},
-        {"step", required_argument, 0, 'x'},
+        {"start", required_argument, 0, 'b'},
+        {"stop", required_argument, 0, 'e'},
+        {"step", required_argument, 0, 'g'},
+        {"seed", required_argument, 0,'s'},
+        {"ratio", required_argument, 0, 'r'},
         {"force", no_argument, 0, 'f'},
         {0, 0, 0, 0}
     };
 
     int opt;
-    int option_index = 0;
-    while ((opt = getopt_long(argc, argv, "i:o:p::s::t::x::f", long_options, &option_index)) != -1) {
-        switch (opt) {
-            case 'i':
-                input_dir = optarg;
-                break;
-            case 'o':
-                output_dir = optarg;
-                break;
-            case 'p':
-                pattern_str = optarg;
-                break;
-            case 's':
-                if (optarg != nullptr) {
-                    start = std::stoul(optarg);
-                }
-                break;
-            case 't':
-                if (optarg != nullptr) {
-                    stop = std::stoul(optarg);
-                }
-                break;
-            case 'x':
-                if (optarg != nullptr) {
-                    step = std::stoul(optarg);
-                }
-                break;
-            case 'f':
-                force = true;
-                break;
-
-            default:
-                std::cerr << "Usage: " << argv[0] << " --in input_dir --out output_dir [--regex pattern] [--start start] [--stop stop] [--step step]\n";
-                return 1;
-        }
-    }
-
-    // Generate downsampling levels based on start, stop, and step values
-    if (start <= 0 || stop <= 0 || step <= 0) {
-        std::cerr << "Warning: either incorrect or missing --start --stop --step arguments. Using the defaults." << std::endl;
-        reads = {100, 200, 300, 400, 500, 1000, 1500, 2000, 3000, 4000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000,
-                 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 95000, 100000, 110000, 120000, 130000, 140000, 150000, 160000, 170000, 180000, 190000, 200000};
-    } else {
-    // Define long-named options
-    static struct option long_options[] = {
-        {"in", required_argument, 0, 'i'},
-        {"out", required_argument, 0, 'o'},
-        {"regex", optional_argument, 0, 'p'},
-        {"start", optional_argument, 0, 'b'},
-        {"stop", optional_argument, 0, 'e'},
-        {"step", optional_argument, 0, 's'},
-        {0, 0, 0, 0}
-    };
-
-    int opt;
-    int option_index = 0;
-    while ((opt = getopt_long(argc, argv, "i:o:p::b::e::x::", long_options, &option_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "i:o:p::b::e::g::s::r::f", long_options, nullptr)) != -1) {
         switch (opt) {
             case 'i':
                 input_dir = optarg;
@@ -201,84 +171,124 @@ int main(int argc, char* argv[]) {
                 pattern_str = optarg;
                 break;
             case 'b':
-                if (optarg != nullptr) {
-                    start = std::stoul(optarg);
-                }
+                start = std::stoul(optarg);
                 break;
             case 'e':
-                if (optarg != nullptr) {
-                    stop = std::stoul(optarg);
-                }
+                stop = std::stoul(optarg);
+                break;
+            case 'g':
+                step = std::stoul(optarg);
                 break;
             case 's':
-                if (optarg != nullptr) {
-                    step = std::stoul(optarg);
-                }
+                seed = std::stoul(optarg);
+                break;
+            case 'r':
+                ratio = std::stod(optarg);
+                break;
+            case 'f':
+                force = true;
                 break;
             default:
                 std::cerr << "Usage: " << argv[0] << " --in input_dir --out output_dir [--regex pattern] [--start start] [--stop stop] [--step step]\n";
                 return 1;
+        }
+    }
+
+    std::cout << "Input directory: " << input_dir << std::endl;
+    std::cout << "Output directory: " << output_dir << std::endl;
+    std::cout << "Regex pattern: " << pattern_str << std::endl;
+
+
+    if(ratio == 0)  {
+        // Generate downsampling levels
+        if (start > 0 && stop > 0 && step > 0) {
+            for (std::size_t i = start; i <= stop; i += step) {
+                reads.push_back(i);
             }
+        } else {
+            std::cerr << "Warning: using default reads." << std::endl;
+            reads = {100, 200, 300, 400, 500, 1000, 1500, 2000, 3000, 4000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000,
+                     55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 95000, 100000, 110000, 120000, 130000, 140000, 150000, 160000, 170000, 180000, 190000, 200000};
         }
-    }
-
-    if (start <= 0 || stop <= 0 || step <= 0) {
-        std::cerr << "Warning: either incorrect or missing --start --stop --step arguments. Using the defaults:" << std::endl;
-        std::cerr << "reads = {100, 200, 300, 400, 500, 1000, 1500, 2000, 3000, 4000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 95000, 10000, 110000, 120000, 130000, 140000, 150000, 160000, 170000, 180000, 190000, 200000}" << std::endl;
-       reads = {100, 200, 300, 400, 500, 1000, 1500, 2000, 3000, 4000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000,
-                 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 95000, 10000, 110000, 120000, 130000, 140000, 150000, 160000, 170000, 180000, 190000, 200000};
     } else {
-        // Generate downsampling levels based on start, stop, and step values
-        for (std::size_t i = start; i <= stop; i += step) {
-            reads.push_back(i);
+        if (ratio > 1) {
+            std::cout << "--ratio/-r passed:" << ratio << " - setting to fractional form: " << 1/ratio << std::endl;
+            ratio = 1/ratio;
         }
     }
 
-    // Regular expression pattern for matching file names
+    // Regular expression for file matching
     std::regex pattern(pattern_str);
+    auto file_names = getFilesInDirectory(input_dir, pattern);
 
-    // Get list of files in the input directory
-    std::vector<std::string> file_names = getFilesInDirectory(input_dir, pattern);
-
-    // Parallel processing using std::async and std::thread
+    // Parallel processing
     std::vector<std::future<void>> futures;
-    std::size_t num_cores = std::thread::hardware_concurrency() - 1;
-
-    std::size_t total_iterations = reads.size() * file_names.size();
+    std::size_t num_cores = std::thread::hardware_concurrency();
     std::size_t completed_iterations = 0;
 
-    for (std::size_t num_reads_to_downsample : reads) {
+    if (ratio == 0) {
+        std::size_t total_iterations = reads.size() * file_names.size();
+        // Cache for original read counts for 'range' mode.
+        std::unordered_map<std::string, std::size_t> read_counts;
+
+        for (std::size_t num_reads_to_downsample : reads) {
+            for (const std::string& file_name : file_names) {
+                std::string output_file_name = output_dir + std::to_string(num_reads_to_downsample) + "_" + file_name.substr(file_name.find_last_of('/') + 1);
+                bool file_exists = (access(output_file_name.c_str(), F_OK) == 0);
+
+                if (!force && file_exists) {
+                    std::cout << "Output file already exists for " << file_name << ", skipping..." << std::endl;
+                    ++completed_iterations;
+                    continue; // Skip processing this iteration
+                }
+
+                if (futures.size() >= num_cores) {
+                    futures.front().wait();
+                    futures.erase(futures.begin());
+                }
+
+                std::cout << "Processing: " << file_name << " (" << num_reads_to_downsample << " reads)" << std::endl;
+                futures.push_back(std::async(std::launch::async, [&]() {
+                    process_file_range(file_name, output_dir, num_reads_to_downsample, read_counts, seed);
+                    ++completed_iterations;
+                    std::cout << "Progress: " << completed_iterations << "/" << total_iterations << " iterations" << std::endl;
+                }));
+            }
+        }
+
+        // Wait for all remaining tasks to complete
+        for (auto& future : futures) {
+            future.wait();
+        }
+    } else {
+        std::size_t total_iterations = file_names.size();
+
         for (const std::string& file_name : file_names) {
-            std::string output_file_name = file_name.substr(file_name.find_last_of('/') + 1); // Get the file name without path
-            output_file_name = output_dir + std::to_string(num_reads_to_downsample) + "_" + output_file_name;
+            std::string output_file_name = output_dir + "ratio=" + std::to_string(ratio) + "_" + file_name.substr(file_name.find_last_of('/') + 1);
             bool file_exists = (access(output_file_name.c_str(), F_OK) == 0);
-            //bool file_exists = std::ifstream(output_file_name).good();
 
             if (!force && file_exists) {
                 std::cout << "Output file already exists for " << file_name << ", skipping..." << std::endl;
                 ++completed_iterations;
                 continue; // Skip processing this iteration
             }
-
             if (futures.size() >= num_cores) {
                 futures.front().wait();
                 futures.erase(futures.begin());
             }
 
-            std::cout << "Processing: " << file_name << " (" << num_reads_to_downsample << " reads)" << std::endl;
-
-            futures.push_back(std::async(std::launch::async, [&completed_iterations, total_iterations, file_name, output_dir, num_reads_to_downsample]() {
-                process_file(file_name, output_dir, num_reads_to_downsample);
+            std::cout << "Processing: " << file_name << " (" << ratio << " ratio)" << std::endl;
+            futures.push_back(std::async(std::launch::async, [&]() {
+                process_file_ratio(file_name, output_dir, ratio, seed);
                 ++completed_iterations;
                 std::cout << "Progress: " << completed_iterations << "/" << total_iterations << " iterations" << std::endl;
             }));
         }
-    }
 
-    // Wait for all remaining tasks to complete
-    for (auto& future : futures) {
-        future.wait();
+        // Wait for all remaining tasks to complete
+        for (auto& future : futures) {
+            future.wait();
+        }
     }
-
     return 0;
 }
